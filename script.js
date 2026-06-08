2eec27c4d689de30d120ead1b4e89b225f1b19ec
Basil Pun <basilpun@Basils-MacBook-Air.local>
2026-06-07 23:43:48 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,117 @@
+let gameScreen;
+let screenWidth = 360;
+let screenHeight = 640;
+let context;
+
+//jellyFish info
+let jellyFishIMG;
+let jwidth = 34;
+let jheight = 24;
+
+let spawnx = screenWidth / 8;
+let spawny = screenHeight / 2;
+
+let jellyFish = {
+    x : spawnx,
+    y : spawny,
+    width : jwidth,
+    height : jheight
+}
+
+//spikes
+let spikes = []
+let spikeWidth = 64
+let spikeHeight = 512
+let spikex = screenWidth
+let spikey = 0
+
+let topSpikeIMG;
+let bottomSpikeIMG;
+
+//game phys
+let velocityX = -2;
+let velocityY = 0; //bird jump speed
+let gravity = 0.4
+
+
+window.onload = function() {
+    const screen = document.querySelector('#gameScreen');
+    screen.height = screenHeight;
+    screen.width = screenWidth;
+    context = screen.getContext("2d");
+    //context.fillRect(jellyFish.x, jellyFish.y, jellyFish.width, jellyFish.height);
+    jellyFishIMG = new Image();
+    jellyFishIMG.src = "imgs/jellyFish.png";
+    jellyFishIMG.onload = function(){
+        context.drawImage(jellyFishIMG, jellyFish.x, jellyFish.y, jellyFish.width, jellyFish.height);
+    }
+
+    topSpikeIMG = new Image();
+    topSpikeIMG.src = "imgs/topSpike.png";
+
+    bottomSpikeIMG = new Image();
+    bottomSpikeIMG.src = "imgs/botSpike.png";
+
+    setInterval(placeSpikes,1500); //every 1.5 sec place spikes
+
+
+    requestAnimationFrame(update);
+
+    document.addEventListener("keydown", moveJF);
+    
+}
+
+//main game loop
+function update(){
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, screenWidth, screenHeight)
+
+    //jellyFish
+    velocityY = velocityY + gravity;
+    jellyFish.y = jellyFish.y + velocityY;
+    context.drawImage(jellyFishIMG, jellyFish.x, jellyFish.y, jellyFish.width, jellyFish.height);
+
+    //spikes
+    for(let i = 0; i < spikes.length; i ++){
+        let spike = spikes[i];
+        spike.x = spike.x + velocityX;
+        context.drawImage(spike.img, spike.x, spike.y, spike.width, spike.height);
+    }
+
+}
+
+function placeSpikes() {
+
+    //spike will be between 1/4 and 3/4 of spike height
+    let randomy = spikey - spikeHeight/4 - Math.random()*(spikeHeight/2);
+
+    let topSpike = {
+        img: topSpikeIMG,
+        x : spikex,
+        y : randomy,
+        width : spikeWidth,
+        height : spikeHeight,
+        passed : false
+    }
+
+    spikes.push(topSpike);
+
+    let openingSpace = screenHeight/8 + 20;
+    
+    let bottomSpike = {
+        img: bottomSpikeIMG,
+        x : spikex,
+        y : randomy + spikeHeight + openingSpace,
+        width : spikeWidth,
+        height : spikeHeight,
+        passed : false
+    }
+
+    spikes.push(bottomSpike);
+}
+
+function moveJF(e){
+    if (e.code == "Space"){
+        velocityY = -6
+    }
+}
